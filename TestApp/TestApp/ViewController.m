2fe5fc2fe3bf20246a48b0274110631b888f1869
wangfg <mac@mactekimacbook-air.local>
2018-03-04 10:22:28 +0800

    GCD的简单使用

--- TestApp/TestApp/ViewController.m
+++ TestApp/TestApp/ViewController.m
@@ -12,6 +12,7 @@
 
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 @property (weak, nonatomic) IBOutlet UITextView *resultTextView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @end
 
@@ -22,6 +23,60 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (NSString *)fetchSomethingFromServer {
+    [NSThread sleepForTimeInterval:1];
+    return @"Hi there";
+}
+
+- (NSString *)processData:(NSString *)data {
+    [NSThread sleepForTimeInterval:2];
+    return [data uppercaseString];
+}
+
+- (NSString *)calculateFirstResult:(NSString *)data {
+    [NSThread sleepForTimeInterval:3];
+    return [NSString stringWithFormat:@"Number of chars: %lu", (unsigned long)[data length]];
+}
+
+- (NSString *)calculateSecondResult:(NSString *)data {
+    [NSThread sleepForTimeInterval:4];
+    return [data stringByReplacingOccurrencesOfString:@"E" withString:@"e"];
+}
+
+- (IBAction)doWork:(id)sender {
+    self.resultTextView.text = @"";
+    NSDate *startTime = [NSDate date];
+    self.startButton.enabled = NO;
+    self.startButton.alpha = 0.5f;
+    [self.spinner startAnimating];
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(queue, ^{
+        NSString *fetchedData = [self fetchSomethingFromServer];
+        NSString *processedData = [self processData:fetchedData];
+//        NSString *firstResult = [self calculateFirstResult:processedData];
+//        NSString *secondResult = [self calculateSecondResult:processedData];
+        __block NSString *firstResult;
+        __block NSString *secondResult;
+        dispatch_group_t group = dispatch_group_create();
+        dispatch_group_async(group, queue, ^{
+            firstResult = [self calculateFirstResult:processedData];
+        });
+        dispatch_group_async(group, queue, ^{
+            secondResult = [self calculateSecondResult:processedData];
+        });
+        dispatch_group_notify(group, queue, ^{
+            NSString *resultSummary = [NSString stringWithFormat:@"First: [%@]\nSecond: [%@]", firstResult, secondResult];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.resultTextView.text = resultSummary;
+                self.startButton.enabled = YES;
+                self.startButton.alpha = 1;
+                [self.spinner stopAnimating];
+            });
+            NSDate *endTime = [NSDate date];
+            NSLog(@"Completed in %f seconds", [endTime timeIntervalSinceDate:startTime]);
+        });
+    });
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
